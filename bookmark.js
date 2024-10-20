0ddea6ab918d76bd9632060a828fe9f1efd3db7b
ahh0619 <86466729+ahh0619@users.noreply.github.com>
2024-10-20 17:56:28 +0900

    북마크 페이지 변수명 변경
    bookmarkState를 bookmarkPageState로 변경.

---
+++ bookmark.js
@@ -0,0 +1,75 @@
+const bookmarkViewBtn = document.querySelector(".bookmark_button");
+const bookmarkReverseBtn = document.querySelector(".reverse_button");
+const bookmarkAddBtn = document.querySelector(".bookmark_addbtn");
+const bookmarkRemoveBtn = document.querySelector(".bookmark_removebtn");
+
+let bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+let bookmarkPageState = false;
+
+function saveBookmarks() {
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+}
+
+function addBookmark(movie) {
+  if (!bookmarks.some((bookmark) => bookmark.id === movie.id)) {
+    bookmarks.push(movie);
+    saveBookmarks();
+    updateBookmarkButtons(movie.id);
+  }
+}
+
+function removeBookmark(movieId) {
+  bookmarks = bookmarks.filter((bookmark) => bookmark.id !== movieId);
+  saveBookmarks();
+  updateBookmarkButtons(movieId);
+
+  if (bookmarkPageState) {
+    renderMovies(bookmarks);
+  }
+}
+
+function updateBookmarkButtons(movieId) {
+  const isBookmarked = bookmarks.some((bookmark) => bookmark.id === movieId);
+  if (isBookmarked) {
+    bookmarkAddBtn.style.display = "none";
+    bookmarkRemoveBtn.style.display = "block";
+  } else {
+    bookmarkAddBtn.style.display = "block";
+    bookmarkRemoveBtn.style.display = "none";
+  }
+}
+
+bookmarkViewBtn.addEventListener("click", () => {
+  bookmarkPageState = true;
+  renderMovies(bookmarks);
+  bookmarkViewBtn.style.display = "none";
+  bookmarkReverseBtn.style.display = "block";
+});
+
+bookmarkReverseBtn.addEventListener("click", () => {
+  bookmarkPageState = false;
+  renderMovies(allMovies);
+  bookmarkViewBtn.style.display = "block";
+  bookmarkReverseBtn.style.display = "none";
+});
+
+bookmarkAddBtn.addEventListener("click", () => {
+  const movieId = parseInt(modalMovieTitle.getAttribute("data-id"));
+  const movie = allMovies.find((movie) => movie.id === movieId);
+  if (movie) {
+    addBookmark(movie);
+  }
+  modalClose();
+  alert("북마크에 추가되었습니다.");
+});
+
+bookmarkRemoveBtn.addEventListener("click", () => {
+  const movieId = parseInt(modalMovieTitle.getAttribute("data-id"));
+  removeBookmark(movieId);
+  modalClose();
+  alert("북마크에서 제거되었습니다.");
+});
+
+window.addEventListener("load", () => {
+  bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+});
